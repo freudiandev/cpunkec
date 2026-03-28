97788468a7e45a1e6d23daf5272d3c2a36317c14
freudiandev <alexalmeida@riseup.net>
2026-03-28 10:06:31 -0500

    v0.0.4

--- docs/dev/static/chunks/src_app_layout_tsx_004glpo._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__02nfq85._.css","static/chunks/node_modules_next_dist_08j_-0b._.js","static/chunks/src_components_Footer_tsx_0743d_g._.js"],
-    source: "dynamic"
-});
