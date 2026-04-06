a34e74ae9c59e9b95d8a4b786af51817d63f090b
freudiandev <rafaelalmeidaorellana@gmail.com>
2026-04-06 17:53:17 -0500

    v.0.1.0

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -1,11 +1,13 @@
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
 const siteUrl =
   process.env.NEXT_PUBLIC_SITE_URL ??
-  `http://localhost:${process.env.PORT ?? "3000"}${basePath}`;
+  (process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}`
+      : `http://localhost:${process.env.PORT ?? "3000"}`);
 
 export const siteConfig = {
   name: "Ciberpunk Ecuador",
-  basePath,
   siteUrl,
-  logoPath: `${basePath}/1.png`,
+  logoPath: "/1.png",
 } as const;
